0333ae5230e980c10fd864b73c1bb988529130d7
wafer-bw <ben.jz.wakeford@gmail.com>
2021-02-15 15:51:42 -0500

    Test Workflow

--- sync/sync.go
+++ sync/sync.go
@@ -5,26 +5,25 @@ import (
 	"os"
 	"strings"
 
+	"github.com/docopt/docopt-go"
 	"github.com/joho/godotenv"
-	"github.com/kelseyhightower/envconfig"
 	"github.com/wafer-bw/udx-discord-bot/commands"
 	"github.com/wafer-bw/udx-discord-bot/disgoslash/app"
 )
 
-type envVars struct {
-	Guilds []string `envconfig:"GUILDS" required:"true" split_words:"true"`
+type appargs struct {
+	GuildIDs []string `docopt:"<guildID>"`
 }
 
-var env envVars
+var usage = `Sync - Reregister Slash Commands with Discord
 
-func init() {
-	if err := godotenv.Load(); err != nil {
-		log.Printf("Warning - %s\n", err)
-	}
-	if err := envconfig.Process("", &env); err != nil {
-		log.Fatalf(err.Error())
-	}
-}
+Usage:
+  sync
+  sync <guildID>...
+  sync -h | --help
+
+Options:
+  -h --help  Show this screen.`
 
 func getErrsStrings(errs []error) []string {
 	errsStr := []string{}
@@ -34,11 +33,33 @@ func getErrsStrings(errs []error) []string {
 	return errsStr
 }
 
+func parseArgs() (*appargs, error) {
+	args := &appargs{}
+	arguments, err := docopt.ParseDoc(usage)
+	if err != nil {
+		return nil, err
+	}
+	if err := arguments.Bind(&args); err != nil {
+		return nil, err
+	}
+	return args, nil
+}
+
+func init() {
+	// Required for running locally but not for workflows
+	if err := godotenv.Load(); err != nil {
+		log.Printf("Warning - %s\n", err)
+	}
+}
+
 func main() {
+	args, err := parseArgs()
+	if err != nil {
+		log.Fatal(err)
+	}
 	syncer := app.NewSyncer()
-	if errs := syncer.Run(env.Guilds, commands.SlashCommandMap); len(errs) > 0 {
+	if errs := syncer.Run(args.GuildIDs, commands.SlashCommandMap); len(errs) > 0 {
 		log.Println(strings.Join(getErrsStrings(errs), "\n"))
 		os.Exit(1)
 	}
-	os.Exit(0)
 }
