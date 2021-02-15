c74d8fd48b76f17abb185b7869256af5ebe914f2
wafer-bw <ben.jz.wakeford@gmail.com>
2021-02-15 15:56:39 -0500

    Remove old todo

--- commands/commands.go
+++ commands/commands.go
@@ -6,8 +6,6 @@ import (
 	"github.com/wafer-bw/udx-discord-bot/disgoslash/slashcommands"
 )
 
-// todo - see if this and the sub directories here can be migrated into the API folder
-
 // SlashCommandMap for the application
 var SlashCommandMap = slashcommands.NewMap(
 	extrinsicrisk.SlashCommand,
