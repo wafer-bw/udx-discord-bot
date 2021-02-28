f46204a5c16cd5c79751445323d4de9337ac7d9a
wafer-bw <ben.jz.wakeford@gmail.com>
2021-02-28 09:55:02 -0500

    Try forwarded for header

--- common/nasdaqapi/nasdaqapi.go
+++ common/nasdaqapi/nasdaqapi.go
@@ -36,7 +36,10 @@ func construct(apiBaseURL string) ClientInterface {
 // GetOptions chains for provided symbol
 func (client *Client) GetOptions(symbol string, assetClass string) (*OptionsResponse, error) {
 	url := fmt.Sprintf("%s/api/quote/%s/option-chain?assetclass=%s&excode=oprac&callput=call&money=in&type=all&fromdate=all", client.apiBaseURL, symbol, assetClass)
-	headers := map[string]string{"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36"}
+	headers := map[string]string{
+		"X-Forwarded-For": "204.101.161.58",
+		"user-agent":      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36",
+	}
 	status, data, err := request.Do(http.MethodGet, url, headers, nil)
 	if err != nil {
 		return nil, err
@@ -54,7 +57,10 @@ func (client *Client) GetOptions(symbol string, assetClass string) (*OptionsResp
 // GetQuote for provided symbol
 func (client *Client) GetQuote(symbol string, assetClass string) (*QuoteResponse, error) {
 	url := fmt.Sprintf("%s/api/quote/%s/info?assetclass=%s", client.apiBaseURL, symbol, assetClass)
-	headers := map[string]string{"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36"}
+	headers := map[string]string{
+		"X-Forwarded-For": "204.101.161.58",
+		"user-agent":      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36",
+	}
 	status, data, err := request.Do(http.MethodGet, url, headers, nil)
 	if err != nil {
 		return nil, err
@@ -75,7 +81,10 @@ func (client *Client) GetGreeks(symbol string, assetClass string, date string) (
 	if date != "" {
 		url += fmt.Sprintf("&date=%s", date)
 	}
-	headers := map[string]string{"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36"}
+	headers := map[string]string{
+		"X-Forwarded-For": "204.101.161.58",
+		"user-agent":      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36",
+	}
 	status, data, err := request.Do(http.MethodGet, url, headers, nil)
 	if err != nil {
 		return nil, err
