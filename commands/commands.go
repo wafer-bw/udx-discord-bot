5805eb8b8a9f925ba0a7bbf68f0f38f2c4106604
wafer-bw <ben.jz.wakeford@gmail.com>
2021-03-28 15:57:10 -0400

    Add missing commands

--- commands/commands.go
+++ commands/commands.go
@@ -4,7 +4,9 @@ import (
 	"github.com/wafer-bw/disgoslash"
 	"github.com/wafer-bw/udx-discord-bot/commands/chstrat"
 	"github.com/wafer-bw/udx-discord-bot/commands/extrinsicrisk"
+	"github.com/wafer-bw/udx-discord-bot/commands/goroutine"
 	"github.com/wafer-bw/udx-discord-bot/commands/helloworld"
+	"github.com/wafer-bw/udx-discord-bot/commands/timeout"
 )
 
 // SlashCommandMap containing slash commands to be deployed and used live
@@ -12,4 +14,6 @@ var SlashCommandMap = disgoslash.NewSlashCommandMap(
 	extrinsicrisk.SlashCommand,
 	helloworld.SlashCommand,
 	chstrat.SlashCommand,
+	timeout.SlashCommand,
+	goroutine.SlashCommand,
 )
