500d65a439f6eb6f806fd6e428ee00e16bd1f55f
wafer-bw <ben.jz.wakeford@gmail.com>
2021-04-18 17:18:11 -0400

    Upgrade disgoslash and remove verify command

--- commands/commands.go
+++ commands/commands.go
@@ -4,12 +4,10 @@ import (
 	"github.com/wafer-bw/disgoslash"
 	"github.com/wafer-bw/udx-discord-bot/commands/chstrat"
 	"github.com/wafer-bw/udx-discord-bot/commands/extrinsicrisk"
-	"github.com/wafer-bw/udx-discord-bot/commands/verify"
 )
 
 // SlashCommandMap containing slash commands to be deployed and used live
 var SlashCommandMap = disgoslash.NewSlashCommandMap(
 	extrinsicrisk.SlashCommand,
 	chstrat.SlashCommand,
-	verify.SlashCommand,
 )
--- go.mod
+++ go.mod
@@ -7,5 +7,5 @@ require (
 	github.com/joho/godotenv v1.3.0
 	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/stretchr/testify v1.7.0
-	github.com/wafer-bw/disgoslash v0.6.2-0.20210418210756-faec7745b86c
+	github.com/wafer-bw/disgoslash v0.7.6
 )
