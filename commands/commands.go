9fc087b0f20d05892fe4362ba208a8b70b9619b9
wafer-bw <ben.jz.wakeford@gmail.com>
2021-04-18 18:16:27 -0400

    switch to ev

--- commands/commands.go
+++ commands/commands.go
@@ -3,13 +3,13 @@ package commands
 import (
 	"github.com/wafer-bw/disgoslash"
 	"github.com/wafer-bw/udx-discord-bot/commands/chstrat"
-	"github.com/wafer-bw/udx-discord-bot/commands/extrinsicrisk"
+	"github.com/wafer-bw/udx-discord-bot/commands/extrinsicvalue"
 	"github.com/wafer-bw/udx-discord-bot/commands/leap"
 )
 
 // SlashCommandMap containing slash commands to be deployed and used live
 var SlashCommandMap = disgoslash.NewSlashCommandMap(
-	extrinsicrisk.SlashCommand,
+	extrinsicvalue.SlashCommand,
 	chstrat.SlashCommand,
 	leap.SlashCommand,
 )
